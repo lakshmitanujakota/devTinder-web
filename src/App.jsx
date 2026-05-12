1336dacfab6bc19ed0e96fe92666ef94642d175b
lakshmitanujakota <tanujakota14@gmail.com>
2026-05-12 20:42:36 +0530

    BrowserRouter react touter dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import { useState } from 'react'
+import Navbar from './NavBar';
+import NavBar from './NavBar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Profile from './Profile';
+import Login from './Login';
+import Body from './Body';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-
+      <BrowserRouter basename="/">
+      <Routes>
+        <Route path="/" element={<Body/>}>
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/Profile" element={<Profile/>}/>
+        </Route>
+      </Routes>
+      </BrowserRouter>  
     </>
   )
 }
