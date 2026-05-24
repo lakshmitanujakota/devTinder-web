fa37b5abe52bec245e86d9f5c43cedcc1f889438
lakshmitanujakota <tanujakota14@gmail.com>
2026-05-24 11:24:01 +0530

    logout

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,23 +3,26 @@ import axios from "axios";
 import { useDispatch } from "react-redux";
 import { addUser } from "../utilis/userSlice";
 import { useNavigate } from "react-router-dom";
-import { BASE_URL} from "../utilis/constants";
+import { BASE_URL } from "../utilis/constants";
 
 const Login = () => {
     const [emailId, setEmail] = useState("rahul@gmail.com");
     const [password, setPassword] = useState("Rahul@123");
     const dispatch = useDispatch();
     const navigate = useNavigate();
+    const[error, setError]=useState("");
 
     const handlelogin = async () => {
-        try {
-            const res = await axios.post(BASE_URL +"/login", { emailId, password },
+        try{
+            const res = await axios.post(BASE_URL + "/login", { emailId, password },
                 { withCredentials: true })
             console.log(res.data);
             dispatch(addUser(res.data));
             navigate("/");
         }
-        catch (err) { console.log(err) }
+        catch(err){ 
+            setError(err?.response?.data || "Something went wrong");
+        }
     }
 
     return (<div className="min-h-screen flex items-center justify-center bg-base-200">
@@ -50,7 +53,7 @@ const Login = () => {
                         onChange={(e) => setPassword(e.target.value)}
                     />
                 </div>
-
+                <p className="text-red-500">{error}</p>
                 <div className="card-actions mt-4">
                     <button className="btn btn-primary w-full" onClick={handlelogin}>Login</button>
                 </div>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,24 @@
-import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utilis/constants";
+import { removeUser } from "../utilis/userSlice";
 
 const NavBar = () => {
 
   const user=useSelector((store)=>store.user);
+  const dispatch=useDispatch();
+  const navigate=useNavigate();
+
+  const handleLogout=async ()=>{
+    try{
+      await axios.post(BASE_URL+"/logout",{},{withCredentials: true});
+      dispatch(removeUser());
+      navigate("login");
+    }catch(err){
+      console.log(err);
+    }
+  }
 
   return (<div className="navbar bg-base-100 shadow-sm">
     <div className="flex-1">
@@ -28,7 +43,7 @@ const NavBar = () => {
             </Link>
           </li>
           <li><a>Settings</a></li>
-          <li><a>Logout</a></li>
+          <li><Link onClick={handleLogout}>Logout</Link></li>
         </ul>
       </div>)}
     </div>
